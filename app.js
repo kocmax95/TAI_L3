c7be08b32af1534593aebf82e764ac725f1faa66
kocmax95 <maksymiliankoczaski@gmail.com>
2018-12-03 16:16:31 +0100

    add quiz

---
+++ app.js
@@ -0,0 +1,102 @@
+function populate(){
+    if(quiz.isEnded()){
+        showScores();
+    }
+    else {
+        // show question
+        var element =document.getElementById("question");
+        element.innerHTML = quiz.getQuestionIndex().text; 
+        
+        // show choices
+        var choices = quiz.getQuestionIndex().choices;
+        for(var i = 0; i<choices.length; i++){
+            var element = document.getElementById("choice" + i);
+            element.innerHTML = choices[i];
+            questions("btn" + i, choices[i]);
+        }
+
+        showProgress();
+    }
+};
+
+function quess(id, quess){
+    var button = document.getElementById(id);
+    button.onclick = function(){
+        quiz.quess(quess);
+        populate();
+    }
+};
+
+function showScores() {
+    var gameOverHtml = "<h1>Result</h1>";
+    gameOverHtml += "<h2 id='score'>Your scores: " + quiz.score + "</h2>";
+    var element = document.getElementById("quiz");
+    element.innerHTML = gameOverHtml;
+};
+
+function showProgress(){
+    var currentQuestionNumber = quiz.questionIndex + 1;
+    var element = document.getElementById("progress");
+    element.innerHTML = "Question" + currentQuestionNumber + " of " + quiz.questions.length;
+}
+
+
+
+
+function populate() {
+    if(quiz.isEnded()) {
+        showScores();
+    }
+    else {
+        // show question
+        var element = document.getElementById("question");
+        element.innerHTML = quiz.getQuestionIndex().text;
+
+        // show options
+        var choices = quiz.getQuestionIndex().choices;
+        for(var i = 0; i < choices.length; i++) {
+            var element = document.getElementById("choice" + i);
+            element.innerHTML = choices[i];
+            guess("btn" + i, choices[i]);
+        }
+
+        showProgress();
+    }
+};
+
+function guess(id, guess) {
+    var button = document.getElementById(id);
+    button.onclick = function() {
+        quiz.guess(guess);
+        populate();
+    }
+};
+
+
+function showProgress() {
+    var currentQuestionNumber = quiz.questionIndex + 1;
+    var element = document.getElementById("progress");
+    element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
+};
+
+function showScores() {
+    var gameOverHTML = "<h1>Result</h1>";
+    gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
+    var element = document.getElementById("quiz");
+    element.innerHTML = gameOverHTML;
+};
+
+// create questions
+var questions = [
+    new Question("Ile jest 1 + 3", ["13", "4", "10", "20"], "4"),
+    new Question("co jest szybsze?", ["motor", "rower", "traktor", "skuter"], "motor"),
+    new Question("jakiego koloru jest słońce?", ["żółte", "zielone", "niebieskie", "czerwone"], "żółte"),
+    new Question("co na skurcze?", ["wapno", "magnez", "Wit C", "Wit A"], "magnez"),
+    new Question("wiecej niż jedno zwierze to: :D", ["stado", "owca", "lama", "pies"], "stado")
+];
+
+// create quiz
+var quiz = new Quiz(questions);
+
+// display quiz
+populate();
